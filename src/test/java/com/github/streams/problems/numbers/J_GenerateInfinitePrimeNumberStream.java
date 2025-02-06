3727713708c46069faa01c347d6af4f38e4e8b39
Rohan Bakhru <rohan_bakhru@epam.com>
2025-02-06 15:39:30 +0530

    I and J solution

--- src/test/java/com/github/streams/problems/numbers/J_GenerateInfinitePrimeNumberStream.java
+++ src/test/java/com/github/streams/problems/numbers/J_GenerateInfinitePrimeNumberStream.java
@@ -1,6 +1,8 @@
 package com.github.streams.problems.numbers;
 
 import com.github.streams.solutions.GeneralNumbersProblemSolution;
+
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
@@ -9,10 +11,19 @@ import org.junit.jupiter.api.Test;
 class J_GenerateInfinitePrimeNumberStream {
 
   @Test
-  @Disabled
   void intStreamOfPrimeNumber() {
     final var mySolution = GeneralNumbersProblemSolution.generateIntStreamOfPrimeNumberStream(5);
-    final var yourSolution = IntStream.range(0, 10);
+    final var yourSolution = IntStream.range(0, Integer.MAX_VALUE)
+            .filter(num -> {
+              if(num <= 1) return false;
+              for(int i= num/2; i>=2; i--) {
+                if(num % i == 0) return false;
+              }
+              return true;
+            })
+            .limit(5)
+            .boxed()
+            .toList();
 
     Assertions.assertEquals(mySolution, yourSolution);
   }
--- src/test/java/com/github/streams/problems/numbers/K_IntArray2DToListConverter.java
+++ src/test/java/com/github/streams/problems/numbers/K_IntArray2DToListConverter.java
@@ -2,6 +2,9 @@ package com.github.streams.problems.numbers;
 
 import com.github.streams.problems.numbers.ignore.data.DummyData;
 import com.github.streams.solutions.GeneralNumbersProblemSolution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +15,9 @@ public class K_IntArray2DToListConverter {
     final int[][] input = DummyData.random2DPrimitiveArray();
 
     var mySolution = GeneralNumbersProblemSolution.convertInt2DArrayToList(input);
-    var yourSolution = List.of(List.<Integer>of());
+    var yourSolution = Arrays.stream(input)
+            .map(arr -> Arrays.stream(arr).boxed().toList())
+            .toList();
 
     Assertions.assertEquals(mySolution, yourSolution);
   }
