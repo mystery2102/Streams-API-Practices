1c96f108a613079789af4a613e447ea3628060cf
Rohan Bakhru <rohan_bakhru@epam.com>
2025-02-06 14:26:23 +0530

    F_SumOfNumbersTest solution

--- src/test/java/com/github/streams/problems/numbers/F_SumOfNumbersTest.java
+++ src/test/java/com/github/streams/problems/numbers/F_SumOfNumbersTest.java
@@ -2,6 +2,8 @@ package com.github.streams.problems.numbers;
 
 import com.github.streams.solutions.GeneralNumbersProblemSolution;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -14,11 +16,10 @@ import org.junit.jupiter.api.Test;
 class F_SumOfNumbersTest {
 
   @Test
-  @Disabled
   void testSumOfUniqueDigitsTest() {
     final var input = List.of(5, 6, 7, 8, 5, 5, 8, 8, 7);
     var mySolution = GeneralNumbersProblemSolution.sumOfNumbers(input);
-    var yourSolution = List.of();
+    var yourSolution = input.stream().mapToInt(i -> i).sum();
 
     Assertions.assertEquals(mySolution, yourSolution);
   }
